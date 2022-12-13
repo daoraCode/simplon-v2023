e00434c62386097ff4e52a762d3b1595d2227d39
daoraCode <severin.mboukou@gmail.com>
2022-12-13 19:45:54 +0100

    update branch files

---
+++ app.js
@@ -0,0 +1,35 @@
+// starting slide index
+let slideIndex = 1;
+
+// n parameter => (slide n1, n2, or n3)
+function plusSlide(n) {
+    showSlide(slideIndex += n)
+}
+
+function currentSlide(n) {
+    showSlide(slideIndex = n)
+}
+
+// main function to change actual slide
+function showSlide(n) {
+    let i;
+    let slides = document.getElementsByClassName("surveyBanner")
+
+    // if (n)slide is higher than the number of slide (array) then
+    // slideIndex == 1
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+
+    // if (n)slide is smaller than the number of slide (array) then
+    // slideIndex == actual number of slides (1, 2 or 3)
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0;i < slides.length;i++) {
+        slides[i].style.display = "none";
+        // console.log(slides[i]);
+    }
+    slides[slideIndex - 1].style.display = "flex";
+}
